5b3edddf2283ddb87c0933e04af28e73dcfabe29
Juan Sebastian Odoñez Sierra <juanseordo2206@outlook.com>
2024-11-21 13:26:19 -0500

    (Back-end): Adding module.proveedores.ts in the file module

---
+++ project-sistema-gestion-basico-backend/src/module/proveedores/proveedores.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProveedoresServices } from './service/proveedores.service';
+import { ProveedoresController } from './controller/proveedores.controller';
+import { Proveedores, ProveedoresSchema } from './schema/proveedores.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Proveedores.name,
+        schema: ProveedoresSchema,
+      },
+    ]),
+  ],
+  controllers: [ProveedoresController],
+  providers: [ProveedoresServices],
+})
+export class ProveedoresModule {}
