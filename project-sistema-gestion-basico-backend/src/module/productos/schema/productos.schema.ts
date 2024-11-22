e2586f1ace08adc8b4728a0fe7a9ce6a6d39ecf2
Juan Sebastian Odoñez Sierra <juanseordo2206@outlook.com>
2024-11-21 23:02:12 -0500

    (Back-end): adding schema productos

---
+++ project-sistema-gestion-basico-backend/src/module/productos/schema/productos.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { IProductos } from '../interface/productos.interface';
+import { Clientes } from 'src/module/clientes/schema/clientes.schema';
+
+@Schema()
+export class Productos extends Document implements IProductos {
+  @Prop({ required: true })
+  nombre_producto: string;
+
+  @Prop({ required: true })
+  cantidad: number;
+
+  @Prop({ required: true })
+  precio: number;
+
+  // Definir como un array de strings que referencia a Proveedores
+  @Prop({ type: [String], ref: 'Proveedores', required: true })
+  proveedor: string[];
+
+  @Prop({ type: [String], ref: 'Clientes', required: true })
+  cliente: string[];
+
+  @Prop({ default: true })
+  activo?: boolean;
+}
+
+export const ProductoSchema = SchemaFactory.createForClass(Productos);
