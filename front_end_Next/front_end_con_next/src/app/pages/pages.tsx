365e22c6b3e0a1be85aebd1db88ffcd97cfe5a5c
Juan Sebastian Odoñez Sierra <juanseordo2206@outlook.com>
2024-11-26 12:15:56 -0500

    feat(Front-end): adding page.tsx file to pages

---
+++ front_end_Next/front_end_con_next/src/app/pages/pages.tsx
@@ -0,0 +1,22 @@
+'use client';
+import React from "react";
+import type { AppProps } from "next/app"; // Importamos el tipo adecuado de Next.js
+import ProveedorLista from "./proveedores/lista/ListaProveedores";
+import styles from "../page.module.css";
+
+export default function page() {
+    return (
+        <div className={styles.page}>
+            {/* Aquí colocamos el Navbar para que aparezca en la página de Home */}
+            
+            <main className={styles.main}>
+                <h1>Welcome to the Home Page</h1>
+                {/* Agrega el contenido de tu página aquí */}
+            </main>
+
+            <footer className={styles.footer}>
+                <p>© 2024 My Application</p>
+            </footer>
+        </div>
+    );
+}
